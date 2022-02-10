0f9cb33065ebe5adbd18a8a6a55d6dc766392035
modifyGB <2782299413@qq.com>
2022-02-10 13:33:17 +0800

    commit

--- crawler/spiders/emote_reply.py
+++ crawler/spiders/emote_reply.py
@@ -4,7 +4,7 @@ Version: 1.0
 Autor: Renhetian
 Date: 2022-02-10 12:44:56
 LastEditors: Renhetian
-LastEditTime: 2022-02-10 13:26:49
+LastEditTime: 2022-02-10 13:33:02
 '''
 
 from crawler.spiders import BaseSpider
@@ -24,8 +24,8 @@ class EmoteReplySpider(BaseSpider):
     def start_requests(self):
         for i in range(1,4):
             yield Request(self.url.format(str(i)))
-        # for i in range(5,360):
-        #     yield Request(self.url.format(str(i)))
+        for i in range(5,360):
+            yield Request(self.url.format(str(i)))
 
     def parse(self, response):
         js = response.json().get('data',{}).get('packages',[{}])[0].get('emote',[])
